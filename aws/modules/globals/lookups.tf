acfe634f11ca845879dafcf048bd4fba9c886667
Brian Burnett <hikerspath@users.noreply.github.com>
2021-04-28 11:54:25 -0400

    Create lookups.tf

---
+++ aws/modules/globals/lookups.tf
@@ -0,0 +1,17 @@
+# DL: Availability Zone List
+data "aws_availability_zones" "all" {}
+
+# DL: Latest Amazon Linux 2 AMI
+data "aws_ami" "amzn" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
